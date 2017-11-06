77af376004b0e0654efaf773e20b6fdc8183a910
Dmitrii <dmitry_cher@mail.ru>
2017-11-06 20:56:13 +0300

    Words reader and writer by byte

--- main/src/main/java/exercises/module4/FileReadByte.java
+++ main/src/main/java/exercises/module4/FileReadByte.java
@@ -1,6 +1,7 @@
 package exercises.module4;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -12,13 +13,21 @@ import java.util.stream.Collectors;
  */
 public class FileReadByte {
 
-    public static Pattern wordPattern = Pattern.compile("[a-zA-Z]");
-    private static final String File =
+    private static Pattern wordPattern = Pattern.compile("[a-zA-Z]");
+    private static final String FILE =
             "C:\\Workspace\\EpamJava\\main\\src\\main\\java\\exercises\\module4\\FileReadByte.java";
+    private static final String NEW_FILE = "C:\\Workspace\\EpamJava\\out\\words.txt";
 
     public static void main(String[] args) {
+        List<String> words = readWordsByByte();
+        Map<String, Integer> sortedMap = sortWordsAndCount(words);
+        writeWords(sortedMap);
+
+    }
+
+    private static List<String> readWordsByByte() {
         List<String> words = new ArrayList<>();
-        try (FileInputStream fileInputStream = new FileInputStream(File)) {
+        try (FileInputStream fileInputStream = new FileInputStream(FILE)) {
             int info;
             Matcher matcher;
             StringBuilder sb = new StringBuilder();
@@ -38,6 +47,10 @@ public class FileReadByte {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return words;
+    }
+
+    private static Map<String, Integer> sortWordsAndCount(List<String> words) {
         Map<String, Integer> wordsMap = new HashMap<>();
         words.forEach(word -> {
                     if (wordsMap.containsKey(word)) {
@@ -49,9 +62,20 @@ public class FileReadByte {
                 }
         );
         Map<String, Integer> sortedMap = wordsMap.entrySet().stream().sorted(Map.Entry.comparingByKey())
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldV, newV) -> oldV, LinkedHashMap::new));
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue, (oldV, newV) -> oldV, LinkedHashMap::new)
+                );
         sortedMap.keySet().forEach(key -> System.out.println(key + " : " + wordsMap.get(key)));
+        return sortedMap;
+    }
 
-
+    private static void writeWords(Map<String, Integer> sortedMap) {
+        try (FileOutputStream out = new FileOutputStream(NEW_FILE)) {
+            for (String key : sortedMap.keySet()) {
+                out.write((key + ":" + sortedMap.get(key) + "\n").getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
